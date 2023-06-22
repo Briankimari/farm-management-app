04f6b631c8fb72ee19f29e6cb52f84c37062e46c
Brian Bandi <briankim7587@gmail.com>
2023-06-23 00:07:49 +0300

    final commits

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalProvider } from './context/Global';
 import authReducer from "./state/index"
 import { configureStore } from '@reduxjs/toolkit';
+import { HashRouter } from 'react-router-dom';
 
 const store= createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
         <GlobalProvider>
-             <App/> 
-        </GlobalProvider>
-      
+            <App/>   
+        </GlobalProvider> 
   
-    </BrowserRouter>
+    </HashRouter>
      </Provider>
 ); 
 
